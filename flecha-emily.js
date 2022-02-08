aea5056e481b3b6d7df9f81392024db5026d2cf2
emilychacon31 <emilychacon31@outlook.com>
2022-02-07 20:43:20 -0600

    agregando archivo flecha-emily.js

---
+++ flecha-emily.js
@@ -0,0 +1,10 @@
+const marcasTenis=["nike","adidas","puma","champion","fila"];
+
+const recibirArreglo=(marcas)=>{
+    marcas.map((marca) => {
+        if(item.length>5){
+            console.log(marca);
+        };
+    });
+};
+recibirArreglo(marcasTenis);
