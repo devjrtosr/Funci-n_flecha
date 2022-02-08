37ff00de5500213c9c2342cbacb7da99a7c426a2
Jaime Santa <Santa0779@gmail.com>
2022-02-07 20:45:47 -0600

    Resolviendo el ejercicio propuesto en el readme

---
+++ flecha-jaime.js
@@ -0,0 +1,15 @@
+
+const funcionflecha = (arreglo)=>{
+     arreglo.map((item)=>{
+
+        if(item.length > 5 ){
+
+            console.log(item);
+
+        }
+    });
+
+
+};
+
+funcionflecha(animales);
