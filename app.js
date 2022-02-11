d1d56ec17a5466da8cd67be2c4934e532b080054
Jlanza12 <lanza3524@gmail.com>
2022-02-10 18:34:27 -0600

    agregando tarea de funcion flecha

---
+++ app.js
@@ -0,0 +1,10 @@
+let EquiposDeFutbol=["Liga", "Cartago", "Saprisa", "Heredia"];
+
+const  recibirArreglo=(equipos) => {
+    EquiposDeFutbol.map(equipo => {
+        if (equipo.Length>5) {
+            console.log(equipo);
+        }
+    } )
+
+}
